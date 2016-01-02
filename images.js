a5f5b8c45feaa53f786e2f2d7663dfeebac79d9a
Ajusa Jain <arhamjain@gmail.com>
2016-01-01 19:24:37 -0500

    Added splashscreen

--- images.js
+++ images.js
@@ -1,1 +1,2 @@
 //This is where we will load all of the images in.
+var splash = new PIXI.Texture.fromImage('/assets/Splashscreen.png');
--- js/app.js
+++ js/app.js
@@ -2,14 +2,28 @@
         window.mozRequestAnimationFrame ||
         window.webkitRequestAnimationFrame ||
         window.msRequestAnimationFrame;
+    window.WebFontConfig = {
+        google: {
+            families: ['VT323::latin']
+        },
 
-    var canvas = document.createElement('canvas'),
-        ctx = canvas.getContext('2d'),
-        container = document.createElement('div'),
-        requestAnimationFrame = window.requestAnimationFrame ||
-        window.mozRequestAnimationFrame ||
-        window.webkitRequestAnimationFrame ||
-        window.msRequestAnimationFrame;
+        active: function() {
+            // do something
+            gameLoop();
+        }
+    };
+
+    // include the web-font loader script
+    /* jshint ignore:start */
+    (function() {
+        var wf = document.createElement('script');
+        wf.src = ('https:' === document.location.protocol ? 'https' : 'http') +
+            '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+        wf.type = 'text/javascript';
+        wf.async = 'true';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(wf, s);
+    })();
     var VERSION = "Alpha 0.1.1",
         WIDTH = 1280,
         HEIGHT = 720,
@@ -25,42 +39,38 @@
         screens = [true, false, false],
         Speed = 6;
     var spellTimer = 0;
-    var stage;
-    var renderer;
-    var titleText;
-    var firstScreen = new PIXI.Container();
-
-    function init() {
-
-        stage = new PIXI.Container();
-
-        // create a renderer instance.
-        renderer = PIXI.autoDetectRenderer(WIDTH, HEIGHT);
-        document.body.appendChild(container);
-        // add the renderer view element to the DOM
-        container.appendChild(renderer.view);
-        // When the connection is open, send some data to the server
-        renderer.view.style.cssText = "border: 1px solid black; width: " + 64 + "%; height: " + 36 + "%;";
-        container.style.cssText = "text-align: center;";
-        canvas.width = WIDTH;
-        canvas.height = HEIGHT;
-        player.id = guid();
-        setTimeout( gameLoop, 1000 );
-        titleText = new PIXI.Text('Spell', {
-        font: '70px VT323',
+    var stage = new PIXI.Container();
+    var renderer = PIXI.autoDetectRenderer(WIDTH, HEIGHT);
+    var container = document.createElement('div');
+    var text = new PIXI.Text(VERSION, {
+        font: '48px VT323',
         fill: 0xff1010,
         align: 'center'
     });
+    text.x = 10;
+    text.y = 10;
+    var splashscreen = new PIXI.Sprite(splash);
+    splashscreen.interactive = true;
+    splashscreen.on('touchstart', gameStart).on('mousedown', gameStart)
+    document.body.appendChild(container);
+    container.appendChild(renderer.view);
+    renderer.view.style.cssText = "border: 1px solid black; width: " + 64 + "%; height: " + 36 + "%;";
+    container.style.cssText = "text-align: center;";
+    player.id = guid();
+    stage.addChild(splashscreen);
+    stage.addChild(text);
+
+    function gameStart() {
+        stage.removeChildren();
+        screens = [false, true, false]
     }
 
     function gameLoop() {
+        if (screens[1]) {
+            //gamelogic
+            console.log("Hey!")
 
-
-        titleText.x = (WIDTH / 2) - 100;
-        titleText.y = (HEIGHT / 2) - 100;
-
-        stage.addChild(titleText);
-
-        renderer.render(stage);
+        };
         requestAnimationFrame(gameLoop);
+        renderer.render(stage);
     }
