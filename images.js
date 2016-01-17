34de818ae77ffb45d3376285a5afa83607ea43a2
Ajusa Jain <arhamjain@gmail.com>
2016-01-17 10:42:28 -0500

    I am trying to fix things

--- images.js
+++ images.js
@@ -1,9 +1,5 @@
 //This is where we will load all of the images in.
-<<<<<<< HEAD
-var splash = new PIXI.Texture.fromImage('assets/Splashscreen.png');
-=======
 var splash = new PIXI.Texture.fromImage('assets/splashscreen.png');
->>>>>>> refs/remotes/origin/master
 var playerImg = new PIXI.Texture.fromImage('assets/wizard.png');
 var manaBar = new PIXI.Texture.fromImage('assets/mana.png');
 var healthBar = new PIXI.Texture.fromImage('assets/health.png');
