1c6d9bb31416bb1260108719f53f73bb04164dff
epicmittmitt <epicmittmitt@yahoo.com>
2016-01-16 20:43:31 -0500

    Merge 63773e18 (new icons) into spell indication code  - Also moved legacy icons into a folder

--- images.js
+++ images.js
@@ -5,7 +5,7 @@ var manaBar = new PIXI.Texture.fromImage('/assets/mana.png');
 var healthBar = new PIXI.Texture.fromImage('/assets/health.png');
 var expBar = new PIXI.Texture.fromImage('assets/exp.png');
 
-var airIcon = new PIXI.Texture.fromImage('assets/elements/air_icon_legacy.png');
-var earthIcon = new PIXI.Texture.fromImage('assets/elements/earth_icon_legacy.png');
-var fireIcon = new PIXI.Texture.fromImage('assets/elements/fire_icon_legacy.png');
-var waterIcon = new PIXI.Texture.fromImage('assets/elements/water_icon_legacy.png');
+var airIcon = new PIXI.Texture.fromImage('assets/elements/air_icon.png');
+var earthIcon = new PIXI.Texture.fromImage('assets/elements/earth_icon.png');
+var fireIcon = new PIXI.Texture.fromImage('assets/elements/fire_icon.png');
+var waterIcon = new PIXI.Texture.fromImage('assets/elements/water_icon.png');
