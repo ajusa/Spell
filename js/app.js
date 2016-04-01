faf687ca492acb92cdddd98ac4d6e1c98d2fef0b
Ajusa Jain <arhamjain@gmail.com>
2016-04-01 16:44:16 -0400

    Structure Update
    I moved some code to the global.js file, and I chained together many of
    the pixi calls to reduce code and make it easier to read.

--- js/app.js
+++ js/app.js
@@ -12,71 +12,11 @@ window.WebFontConfig = {
         gameLoop();
     }
 };
-
-// include the web-font loader script
-/* jshint ignore:start */
-(function() {
-    var wf = document.createElement('script');
-    wf.src = ('https:' === document.location.protocol ? 'https' : 'http') +
-        '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-    wf.type = 'text/javascript';
-    wf.async = 'true';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(wf, s);
-})();
-var spelldata;
-marmottajax("js/spelldata.json").then(function(content) {
-    spelldata = JSON.parse(content);
-    spelldata = spelldata.spelldata;
-    // console.log(spelldata);
-});
-var player;
-var Players = [];
-var VERSION = "Alpha 0.1.3",
-    WIDTH = 1280,
-    HEIGHT = 720,
-    GROUND = {
-        x: 0,
-        y: HEIGHT - 50,
-        width: WIDTH,
-        height: 50
-    };
-var Spells = [],
-    screens = [true, false, false],
-    Speed = 6;
-var spellTimer = 0;
-var stage = new PIXI.Container();
-var renderer = PIXI.autoDetectRenderer(WIDTH, HEIGHT);
-var container = document.createElement('div');
-var text = new PIXI.Text(VERSION, {
-    font: '48px VT323',
-    fill: 0xff1010,
-    align: 'center'
-});
-text.x = 10;
-text.y = 10;
-var splashscreen = new PIXI.Sprite(splash);
 splashscreen.interactive = true;
 splashscreen.on('touchstart', gameStart).on('mousedown', gameStart)
-document.body.appendChild(container);
-container.appendChild(renderer.view);
-renderer.view.style.cssText = "border: 1px solid black; width: " + 64 + "%; height: " + 36 + "%;";
-container.style.cssText = "text-align: center;";
-stage.addChild(splashscreen);
-stage.addChild(text);
-setInterval(function() {
-    if (screens[1]) player.regen();
-}, 1000);
+// include the web-font loader script
+/* jshint ignore:start */
 
-var multi;
-
-var filterStrength = 10;
-var frameTime = 0,
-    lastLoop = new Date,
-    thisLoop;
-
-var biasStrength = 0.003;
-var spellIconMap;
 function gameStart() {
     multi = new Multiplayer();
     player = new Player(WIDTH / 2 - 25, 450, 150, 232)
@@ -85,21 +25,11 @@ function gameStart() {
     stage.removeChildren();
     screens = [false, true, false];
     bg = new PIXI.Graphics();
-    bg.beginFill('0x34495e');
-    bg.drawRect(0, 0, WIDTH, HEIGHT);
-    bg.endFill();
-    bg.beginFill('0x95a5a6');
-    bg.drawRect(GROUND.x, GROUND.y, GROUND.width, GROUND.height);
-    bg.endFill();
-    bg.beginFill('0x660000');
-    bg.drawRect(0, 10, WIDTH / 2, 15);
-    bg.endFill();
-    bg.beginFill('0x141452');
-    bg.drawRect(WIDTH / 2, 10, WIDTH / 2, 15);
-    bg.endFill();
-    bg.beginFill('0x006600');
-    bg.drawRect(0, 0, WIDTH, 10);
-    bg.endFill();
+    bg.beginFill('0x34495e').drawRect(0, 0, WIDTH, HEIGHT).endFill();
+    bg.beginFill('0x95a5a6').drawRect(GROUND.x, GROUND.y, GROUND.width, GROUND.height).endFill();
+    bg.beginFill('0x660000').drawRect(0, 10, WIDTH / 2, 15).endFill();
+    bg.beginFill('0x141452').drawRect(WIDTH / 2, 10, WIDTH / 2, 15).endFill();
+    bg.beginFill('0x006600').drawRect(0, 0, WIDTH, 10).endFill();
     bg.cacheAsBitmap = true; // temporary for less resource usage
     stage.addChild(bg);
     stage.addChild(player.sprite);
@@ -270,7 +200,6 @@ function gameLoop() {
     var thisFrameTime = (thisLoop = new Date) - lastLoop;
     frameTime += (thisFrameTime - frameTime) / filterStrength;
     lastLoop = thisLoop;
-
     requestAnimationFrame(gameLoop);
     renderer.render(stage);
 }
