223f433e1028023008db2120f9ae227ab07bbe14
Ajusa Jain <arhamjain@gmail.com>
2016-04-15 21:36:01 -0400

    Added a "death"
    Basically, the player disconnects on death now.
    So yeah

--- js/entities/spells.js
+++ js/entities/spells.js
@@ -1,10 +1,11 @@
-function Spell(xval, yval, spd, spell, id) {
+function Spell(xval, yval, spell, id, sign) {
     this.x = xval;
     this.y = yval;
     this.width = 24; // 20
     this.height = 24; // 10
-    this.speed = spd;
+    this.speed = sign * spelldata.spells[spell].speed;
     this.id = id;
+    this.damage = spelldata.spells[spell].damage;
     this.sprite = new PIXI.Sprite.fromImage(spelldata.spells[spell].spritepath);
     stage.addChild(this.sprite);
     this.update = function(i) {
